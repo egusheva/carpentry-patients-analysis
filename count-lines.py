a555badce467c8136a0c1dcb14262c90eb5d0121
egusheva <emaguseva@gmail.com>
2021-11-18 13:28:17 +0100

    added a description of expected input

--- count-lines.py
+++ count-lines.py
@@ -1,3 +1,6 @@
+"""This module counts the number of lines in  a standard input.
+Input: A string from the system's standard input"""
+
 import sys
 
 count=0
@@ -5,3 +8,4 @@ count=0
 for line in sys.stdin:
 	count+=1
 print(count,"lines in a standard input")
+
